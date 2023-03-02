2987a7e0b4687439b5c10b1ad78ae48f3422e89a
Prakash <prakash.gupta1994@gmail.com>
2023-03-02 18:25:59 +0530

    Commit

--- Questions.js
+++ Questions.js
@@ -63,3 +63,36 @@ const inverseLeftTriangle = () => {
 var a = 5;
 let a = 6;
 console.log(a);
+
+
+
+
+//print the "abcdefghijk" like this "abdcefhgijk"
+
+let str = "abcdefghijk";
+let outStr = "";
+let canSkip = false;
+let firstBlock = false;
+for (let i = 0; i < str.length; i++) {
+  if (i % 2 !== 0 || i == 0) {
+    if (canSkip) {
+      outStr += str[i + 1];
+      canSkip = false;
+      firstBlock = true;
+      continue;
+    }
+    outStr += str[i];
+  } else {
+    if (firstBlock) {
+      firstBlock = false;
+      continue;
+    }
+    outStr += str[i + 1] ? str[i + 1] : str[i];
+    if (str[i + 1]) {
+      outStr += str[i];
+    }
+    canSkip = true;
+  }
+}
+
+console.log(outStr);
